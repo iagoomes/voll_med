12f332aca8edc16d29da3590eb9c2c7c1ffea400
iagoomes <iagoomes@outlook.com>
2023-06-27 10:07:27 -0300

    Fluxo de login criado

--- api/src/main/java/br/com/iagoomes/voll/med/api/controller/AutenticacaoController.java
+++ api/src/main/java/br/com/iagoomes/voll/med/api/controller/AutenticacaoController.java
@@ -1,6 +1,8 @@
 package br.com.iagoomes.voll.med.api.controller;
 
 import br.com.iagoomes.voll.med.api.domain.usuario.AutenticacaoRequest;
+import br.com.iagoomes.voll.med.api.domain.usuario.Usuario;
+import br.com.iagoomes.voll.med.api.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,9 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/login")
 public class AutenticacaoController {
     private final AuthenticationManager manager;
+    private final TokenService tokenService;
 
-    public AutenticacaoController(AuthenticationManager manager) {
+    public AutenticacaoController(AuthenticationManager manager, TokenService tokenService) {
         this.manager = manager;
+        this.tokenService = tokenService;
     }
 
     @PostMapping
@@ -26,6 +30,6 @@ public class AutenticacaoController {
                 new UsernamePasswordAuthenticationToken(autenticacaoRequest.login(), autenticacaoRequest.senha());
         Authentication authenticate = manager.authenticate(token);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(tokenService.gerartoken((Usuario) authenticate.getPrincipal()));
     }
 }
---
+++ api/src/main/java/br/com/iagoomes/voll/med/api/infra/security/TokenService.java
@@ -0,0 +1,32 @@
+package br.com.iagoomes.voll.med.api.infra.security;
+
+import br.com.iagoomes.voll.med.api.domain.usuario.Usuario;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    public String gerartoken(Usuario usuario) {
+        try {
+            Algorithm algoritimo = Algorithm.HMAC256("12345678");
+            return JWT.create()
+                    .withIssuer("API Voll.med")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algoritimo);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("erro ao gerar token jwt", exception);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
